e05148c32fbb29fe12d32030d55ce9cca84d5db3
Ivan Hernandez <Ivanh3486@gmail.com>
2026-03-29 22:10:34 -0700

    Added sqrs.cpp program and debugging fix

---
+++ chapter-04-Arrays,String,andpointers/NullTerm.cpp
@@ -0,0 +1,24 @@
+/*
+folowing program converts a string to uppercase
+*/
+
+#include <iostream>
+#include <cstring>
+#include <cctype>
+using namespace std;
+
+
+int main()
+{
+
+    char str[80];
+    int i;
+
+    strcpy(str, "this is a test");
+
+    for(i=0; str[i]; i++) str[i] = toupper(str[i]);
+
+    cout << str;
+
+    return 0;
+}
